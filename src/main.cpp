779d134a7518f254f4474663d900d0a7ebf808a1
robinZERTY <robinlecouffe@gmail.com>
2024-12-26 14:19:36 +0100

    sadly move Player instances to main.cpp because of using vector in BeamSwitch constructors

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include "PING.hpp"
 
+Player PING::player1 = Player(P1_STEP_PIN, P1_DIR_PIN, TMC1_ADDRESS, P1_SOLENOID_PIN, P1_BEAM_R_PIN);
+Player PING::player2 = Player(P2_STEP_PIN, P2_DIR_PIN, TMC2_ADDRESS, P2_SOLENOID_PIN, P2_BEAM_R_PIN);
+Player PING::player3 = Player(P3_STEP_PIN, P3_DIR_PIN, TMC3_ADDRESS, P3_SOLENOID_PIN, P3_BEAM_R_PIN);
+Player PING::player4 = Player(P4_STEP_PIN, P4_DIR_PIN, TMC4_ADDRESS, P4_SOLENOID_PIN, P4_BEAM_R_PIN);
+
 void setup(){PING::setup();}
 
 void loop(){}
