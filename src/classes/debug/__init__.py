62aafeca956516016232762c323e69838baca144
robinAZERTY <robinlecouffe@gmail.com>
2025-01-12 22:53:53 +0100

    fix: update logFile import for main module compatibility

--- src/classes/debug/__init__.py
+++ src/classes/debug/__init__.py
@@ -1,3 +1,6 @@
-from logFile import logFile
-
-logger = logFile()
+if __name__ == '__main__':
+    from logFile import LogFile
+else:
+    from classes.debug.logFile import LogFile
+    
+logger = LogFile()
