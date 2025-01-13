18476472f65ed85eef1e47d25dd3477e2d4a00d8
Simon.Wavrant <wavrantsim@gmail.com>
2025-01-13 17:43:33 +0100

    feat: refactor la classe GameMode et ajouter les classes de sortie pour le joueur, le bumper et le strip LED

--- src/pingpy/output/output.py
+++ src/pingpy/output/output.py
@@ -1,7 +1,10 @@
-from ..hardware.ledStrip import LedStrip, PlayerLedStrip
+from pingpy.debug import logger
+from pingpy.output.player import PlayerOutput
+from pingpy.output.speaker import SpeakerOutput
 
 class Output:
     def __init__(self):
-        pass
-
+        self.player = [PlayerOutput() for _ in range (4)]
+        self.speaker = SpeakerOutput()
+        logger.write_in_log("INFO", __name__, "__init__")
 
