8d8a491a4285bd9f591d82d48234314e13f0a7f0
robinAZERTY <robinlecouffe@gmail.com>
2025-01-14 14:43:42 +0100

    Add initial implementation of pingpy module with game modes and input/output handling

---
+++ raspberry/src/pingpy/input/gameController3Button.py
@@ -0,0 +1,7 @@
+class GameController3ButtonInput(GameControllerInput):
+    def __init__(self, newAction, newActionLeft, newActionRight, newActionShoot):
+        self.newAction=newAction
+        self.newActionleft=newActionLeft
+        self.newActionright=newActionRight
+        self.newActionShoot=newActionShoot
+    pass
