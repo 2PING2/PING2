f980c71264f025c8bb80e779cafd2cd3fbdb02bd
robinAZERTY <robinlecouffe@gmail.com>
2025-01-15 15:40:23 +0100

    Add git update check in hotspot service monitoring

--- raspberry/src/pingpy/gitUpdate/hotspot.py
+++ raspberry/src/pingpy/gitUpdate/hotspot.py
@@ -98,6 +98,7 @@ class Hotspot:
             if self.check_wifi():
                 self.should_stop = True
                 self.stop_services()  # Stop services if connected
+                self.check_git_update()
                 logger.write_in_log("INFO", __name__, "monitor_services", "Wi-Fi setup successful")
                 return
             time.sleep(CHECK_WIFI_DELAY)  # Check connection
