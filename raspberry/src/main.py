ffb8c52c27db11a87e374fa6638ff3132076b73b
robinAZERTY <robinlecouffe@gmail.com>
2025-01-15 16:25:26 +0100

    Enhance error handling in main.py by improving backup restoration process and logging errors

--- raspberry/src/main.py
+++ raspberry/src/main.py
@@ -1,25 +1,25 @@
-# try :
-# import adeiojf
-from pingpy import ping
+try :
+    import adeiojf
+    from pingpy import ping
 
-ping.setup()
+    ping.setup()
 
-while True:
-    ping.run()
+    while True:
+        ping.run()
         
   
-# except Exception as e:
-#     print("Error in the main.py")
-#     import os
-#     # restore from the backup
-#     os.system(f'rm -r /home/pi/Documents/PING2/*')
-#     print("PING2 folder deleted")
-#     os.system(f'cp -r /home/pi/Documents/backup/* /home/pi/Documents/PING2/')
-#     print("PING2 folder restored from the backup")
-#     try:
-#         import pingpy.logger as logger
-#         logger.write_in_log("ERROR", __name__, "main", str(e))
-#     except:
-#         pass
-#     os.system(f'sleep 1 && python3 /home/pi/Documents/PING2/raspberry/src/main.py')
-#     exit(1) 
+except Exception as e:
+    print("Error in the main.py")
+    import os
+    # restore from the backup
+    os.system(f'rm -rf /home/pi/Documents/PING2/* /home/pi/Documents/PING2/.*')
+    print("PING2 folder deleted")
+    os.system(f'cp -r /home/pi/Documents/backup/. /home/pi/Documents/PING2/')
+    print("PING2 folder restored from the backup")
+    try:
+        import pingpy.logger as logger
+        logger.write_in_log("ERROR", __name__, "main", str(e))
+    except:
+        pass
+    os.system(f'sleep 1 && python3 /home/pi/Documents/PING2/raspberry/src/main.py')
+    exit(1) 
