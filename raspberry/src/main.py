161da53fc3829ab18a8976bcd899616b619e42e4
robinAZERTY <robinlecouffe@gmail.com>
2025-01-15 16:04:10 +0100

    Refine backup restoration process in main.py by modifying the deletion command to clear the PING2 folder contents before restoration

--- raspberry/src/main.py
+++ raspberry/src/main.py
@@ -3,7 +3,7 @@ try :
     from pingpy import ping
 
     ping.setup()
-
+    
     while True:
         ping.run()
         
@@ -12,9 +12,9 @@ except Exception as e:
     print("Error in the main.py")
     import os
     # restore from the backup
-    os.system(f'rm -r /home/pi/Documents/PING2')
+    os.system(f'rm -r /home/pi/Documents/PING2/*')
     print("PING2 folder deleted")
-    os.system(f'cp -r /home/pi/Documents/backup* /home/pi/Documents/PING2')
+    os.system(f'cp -r /home/pi/Documents/backup/* /home/pi/Documents/PING2/')
     print("PING2 folder restored from the backup")
     try:
         import pingpy.logger as logger
