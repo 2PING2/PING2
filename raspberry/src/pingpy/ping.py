8144fd32d56107b6beae1bb26eb2127bb2e3447f
robinAZERTY <robinlecouffe@gmail.com>
2025-01-16 10:33:51 +0100

    Add blue color definition and update player LED strip initialization

--- raspberry/src/pingpy/ping.py
+++ raspberry/src/pingpy/ping.py
@@ -1,5 +1,5 @@
 from pingpy import *
-from pingpy.config.config import BAUD_RATE, TIMEOUT, ports, PLAYER_LED_STRIP_OFFSETS
+from pingpy.config.config import BAUD_RATE, TIMEOUT, ports, PLAYER_LED_STRIP_OFFSETS, GREEN, ORANGE, YELLOW, RED, BLUE
 from pingpy.input import Input
 from pingpy.output import Output
 from pingpy.debug import logger
@@ -15,7 +15,8 @@ class Ping:
         self.esp32 = serialHard.ESP32Serial(ports["ESP32"], BAUD_RATE, TIMEOUT)
         self.UICorner = serialHard.UICornerSerial(ports["UICorner"], BAUD_RATE, TIMEOUT)
         self.currentGameMode = WaitingRoom()
-        self.player1LedStrip = PlayerLedStrip(ledStrip, PLAYER_LED_STRIP_OFFSETS[1])
+        # self.player1LedStrip = PlayerLedStrip(ledStrip, PLAYER_LED_STRIP_OFFSETS[1])
+        self.playerLedStrip = [PlayerLedStrip(ledStrip, PLAYER_LED_STRIP_OFFSETS[i+1]) for i in range(4)]
         logger.write_in_log("INFO", __name__, "__init__")
         
     def setup(self):
@@ -23,16 +24,21 @@ class Ping:
         self.UICorner.setup()
         ledStrip.setup()
         ledStrip.clear()
+        self.playerLedStrip[0].onPlayer(YELLOW)
+        self.playerLedStrip[1].onPlayer(GREEN)
+        self.playerLedStrip[2].onPlayer(RED)
+        self.playerLedStrip[3].onPlayer(BLUE)
         logger.write_in_log("INFO", __name__, "setup")
         
     def select_game_mode(self):
         pass
     
     def run(self):
-        self.esp32.read(self.input)
+        # self.esp32.read(self.input)
         # self.UiCorner.read(self.input)
         # self.currentGameMode.compute(self.input, self.output)
         # self.refresh_output()
+        pass
     
     def refresh_output(self):
         pass
