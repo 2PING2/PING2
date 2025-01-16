c33bc798df20a816f74dc02a33ba18d773a21c29
robinAZERTY <robinlecouffe@gmail.com>
2025-01-16 11:34:38 +0100

    Reset initial LED brightness to 0.0 and update brightness calculation for consistency

--- raspberry/src/pingpy/gameMode/waitingRoom.py
+++ raspberry/src/pingpy/gameMode/waitingRoom.py
@@ -11,7 +11,7 @@ class WaitingRoom(GameMode):
     """
     def __init__(self):
         logger.write_in_log("INFO", __name__, "__init__")
-        self.currentLed_brightness = 0.5
+        self.currentLed_brightness = 0.0
         self.last_time = time.time()
         
     def setup(self, output):
@@ -22,15 +22,15 @@ class WaitingRoom(GameMode):
     
     def compute(self, input, output):
         for i in range(4):
-            output.player[i].playerLedStrip.color = tuple(x * self.currentLed_brightness for x in PURPLE)
+            output.player[i].playerLedStrip.color = tuple(round(x * self.currentLed_brightness) for x in PURPLE)
         t = time.time()
         dt = t - self.last_time
         self.last_time = t
         
-        # self.currentLed_brightness += 0.1 * float(dt)
-        # if self.currentLed_brightness > 1:
-        #     self.currentLed_brightness = 0.0
-        # self.currentLed_brightness = (self.currentLed_brightness + 0.1 * float(dt)) % 1.0
+        self.currentLed_brightness += 0.1 * float(dt)
+        if self.currentLed_brightness > 1:
+            self.currentLed_brightness = 0.0
+        self.currentLed_brightness = (self.currentLed_brightness + 0.1 * float(dt)) % 1.0
     
 
     def stop(self, input, output):
