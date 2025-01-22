445e64c621790db54aabff42bd6a4e884334cb86
robinAZERTY <robinlecouffe@gmail.com>
2025-01-22 18:14:46 +0100

    fix: add stop attribute to LinearActuatorOutput class

--- raspberry/src/pingpy/output/linearActuator.py
+++ raspberry/src/pingpy/output/linearActuator.py
@@ -6,5 +6,6 @@ class LinearActuatorOutput:
         self.moveToLeft = None
         self.moveTo = None
         self.setSpeed = None
+        self.stop = None
         logger.write_in_log("INFO", __name__, "__init__")
     pass
