5517992c9aa047995934b2b0eccf973f4aff18ed
robinAZERTY <robinlecouffe@gmail.com>
2025-01-22 11:56:59 +0100

    fix: ensure serial port is only opened if not already open in setup method

--- raspberry/src/pingpy/serialHard/serialCom.py
+++ raspberry/src/pingpy/serialHard/serialCom.py
@@ -51,7 +51,8 @@ class SerialCom:
                 logger.write_in_log("ERROR", __name__, "setup", f"Error opening port l51 {self.symlink}: {e}")
                 return
             # make sure the Serial is closed at the beginning
-            self.ser.open()
+            if not self.ser.is_open:
+                self.ser.open()
             logger.write_in_log("INFO", __name__, "setup", f"Connected to symlink {self.symlink} at {self.baudrate} baud")   
         except Exception as e:
             logger.write_in_log("ERROR", __name__, "setup", f"Error opening port {self.symlink}: {e}")
