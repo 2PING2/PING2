54b99ab7df3d99ac6beb311f359681c637113c83
Simon.Wavrant <wavrantsim@gmail.com>
2025-01-29 11:27:51 +0100

    fix: ajouter un drapeau pour le mode de jeu pré-sélectionné dans waitingRoom.py et corriger l'initialisation de pygame.mixer dans speaker.py

--- src/pingpy/output/speaker.py
+++ src/pingpy/output/speaker.py
@@ -2,21 +2,16 @@ from pingpy.debug import logger
 import os
 os.environ["SDL_AUDIODRIVER"] = "alsa"
 import pygame
-print("SDL_AUDIODRIVER =", os.environ.get("SDL_AUDIODRIVER"))
 
-try:
-    pygame.mixer.init()
-except Exception as e:
-    logger.write_in_log("ERROR", __name__ , "Error in initializing audio:{}".format(e))
-    
+ 
 class SpeakerOutput:
     def __init__(self):
         self.audioPiste = None
         self.isBusy = False
-        # try:
-        #     pygame.mixer.init()
-        # except Exception as e:
-        #     logger.write_in_log("ERROR", __name__ , "Error in initializing audio:{}".format(e))
+        try:
+            pygame.mixer.init()
+        except Exception as e:
+            logger.write_in_log("ERROR", __name__ , "Error in initializing audio:{}".format(e))
         logger.write_in_log("INFO", __name__, "__init__")
 
     def play(self):
@@ -34,8 +29,7 @@ class SpeakerOutput:
             pygame.mixer.music.play()
             self.audioPiste = None
             logger.write_in_log("INFO", __name__, "Playing audio:{}".format(self.audioPiste))
-            # sound = AudioSegment.from_file(self.audioPiste)
-            # _play_with_ffplay(sound)
+            
             # self.isBusy = False
 
         except FileNotFoundError:
