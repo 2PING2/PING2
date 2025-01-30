7d062076ae59d5bfdae4b8d3daedc224e71db3fa
Simon.Wavrant <wavrantsim@gmail.com>
2025-01-30 17:15:16 +0100

    fix: améliorer la logique de gestion des LED de statut dans UICorner

--- raspberry/src/pingpy/serialHard/UICorner.py
+++ raspberry/src/pingpy/serialHard/UICorner.py
@@ -80,12 +80,12 @@ class UICornerSerial(SerialCom):
         if output_ptr.UICorner.askForStatusSettings:
             output_ptr.UICorner.askForStatusSettings = None
             self.send_data(ASK_STATUS_SETTINGS + SEP_KEY)
-        if output_ptr.UICorner.statusLed:
-            output_ptr.UICorner.statusLed = None
-            self.send_data(STATUS_LED_KEY + SEP_KEY + STATUS_LED_ON)
-        if not output_ptr.UICorner.statusLed:
-            logger.write_in_log("INFO", __name__, "write", "status led off{}".format(output_ptr.UICorner.statusLed))
-            output_ptr.UICorner.statusLed = None
-            self.send_data(STATUS_LED_KEY + SEP_KEY + STATUS_LED_OFF)
+        if not output_ptr.UICorner.statusLed is None:
+            if output_ptr.UICorner.statusLed:
+                output_ptr.UICorner.statusLed = None
+                self.send_data(STATUS_LED_KEY + SEP_KEY + STATUS_LED_ON)
+            if not output_ptr.UICorner.statusLed:
+                output_ptr.UICorner.statusLed = None
+                self.send_data(STATUS_LED_KEY + SEP_KEY + STATUS_LED_OFF)
                 
             
