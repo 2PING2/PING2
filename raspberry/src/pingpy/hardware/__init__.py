c7f4b32833d7e8c56c12f611b3969054272353d2
Simon.Wavrant <wavrantsim@gmail.com>
2025-01-31 16:03:11 +0100

    fix: corriger l'importation de MAX_BRIGHTNESS dans __init__.py

--- raspberry/src/pingpy/hardware/__init__.py
+++ raspberry/src/pingpy/hardware/__init__.py
@@ -1,5 +1,5 @@
 from .autoSwitch import AutoSwitch
 from .ledStrip import LedStrip
-from pingpy.config.config import LED_STRIP_PIN, NUMBER_OF_LEDS, FREQUENCY, DMA_CHANNEL, MAX_BRIGTHNESS
+from pingpy.config.config import LED_STRIP_PIN, NUMBER_OF_LEDS, FREQUENCY, DMA_CHANNEL, MAX_BRIGHTNESS
 
-ledStrip = LedStrip(LED_STRIP_PIN, NUMBER_OF_LEDS, FREQUENCY, DMA_CHANNEL, MAX_BRIGTHNESS)
+ledStrip = LedStrip(LED_STRIP_PIN, NUMBER_OF_LEDS, FREQUENCY, DMA_CHANNEL, MAX_BRIGHTNESS)
