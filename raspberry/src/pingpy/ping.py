b072bc3d51fc90fb8bf87ed77c5fc0dacb8eab64
Simon.Wavrant <wavrantsim@gmail.com>
2025-01-31 16:04:51 +0100

    fix: mettre à jour la luminosité des bandes LED en utilisant la valeur d'entrée UICorner

--- raspberry/src/pingpy/ping.py
+++ raspberry/src/pingpy/ping.py
@@ -76,5 +76,5 @@ class Ping:
     def refresh_player_led_strip(self):
         for i in range(4):
             self.playerLedStrip[i].set_mm(self.output.player[i].playerLedStrip.area, self.output.player[i].playerLedStrip.color) 
-            self.playerLedStrip[i].set_brightness(self.output.player[i].playerLedStrip.brightness)
+            self.playerLedStrip[i].set_brightness(self.input.player[i].UICorner.light)
         ledStrip.show()
