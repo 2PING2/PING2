b67b2b7b049c0e80d8636333ae5c7623dae6f281
Simon.Wavrant <wavrantsim@gmail.com>
2025-02-05 16:28:51 +0100

    feat: ajouter le mode de jeu SandBox avec configuration audio et gestion des actions des joueurs

--- src/pingpy/ping.py
+++ src/pingpy/ping.py
@@ -16,7 +16,7 @@ class Ping:
         self.output = Output()
         self.esp32 = serialHard.ESP32Serial(ports["ESP32"], BAUD_RATE, TIMEOUT)
         self.UICorner = serialHard.UICornerSerial(ports["UICorner"], UI_CORNER_BAUD_RATE, TIMEOUT)
-        self.gameModeList = [RedLightGreenLight()]
+        self.gameModeList = [RedLightGreenLight(), SandBox()]
         self.currentGameMode = None
         self.waitingRoom = WaitingRoom(self.gameModeList, self.currentGameMode)
         self.currentGameMode = self.waitingRoom
