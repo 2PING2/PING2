b75b98b0927b34134fae032cfe043eca5ee032da
robinAZERTY <robinlecouffe@gmail.com>
2025-02-06 16:01:29 +0100

    refactor: enhance actuator calibration and movement logic for player3

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -9,11 +9,20 @@ void setup()
     Serial.begin(115200);
     PING::setup();
     // Serial.println("hello from PING esp32");
-    // Serial.println("Calibrating actuators...");
-    // PING::player1.actuator.calibrate();
+    Serial.println("Calibrating actuators...");
+    PING::player3.actuator.calibrate();
+    while(!PING::player3.actuator.calibration()){PING::player3.actuator.run();}
     // PING::player2.actuator.calibrate();
     // PING::player3.actuator.calibrate();
     // PING::player4.actuator.calibrate();
+    PING::player3.actuator.set_acceleration(LINEAR_ACTUATOR_MAX_ACCELERATION);
+    PING::player3.actuator.move_to(100);
+    for (int i = 0; i < 10; i++)
+    {
+    while(PING::player3.actuator.run()){}
+    PING::player3.actuator.set_max_speed(50 + PING::player3.actuator.max_speed());
+    PING::player3.actuator.move_to(-PING::player3.actuator.current_position());
+    }
 
 }
 
