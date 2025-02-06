034da6601c711270f478925558290c44419e569e
robinAZERTY <robinlecouffe@gmail.com>
2025-02-06 22:01:15 +0100

    refactor: replace AccelStepper with FastAccelStepper and update motor control methods

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -2,18 +2,12 @@
 #include <Arduino.h>
 #include "PING.hpp"
 
-Player *players[4] = {&PING::player1, &PING::player2, &PING::player3, &PING::player4};
-
 void setup()
 {
     Serial.begin(115200);
     PING::setup();
-    // Serial.println("hello from PING");
 }
 
 void loop()
 {
-    for (LinearActuator *la : LinearActuator::all)
-        la->run();
 }
-
