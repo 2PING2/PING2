bfff0d128749edd90c3863685eddb09f34719d30
robinAZERTY <robinlecouffe@gmail.com>
2025-02-06 11:04:15 +0100

    refactor: remove redundant print statements in lookForSshIp and sendStatus methods

--- raspberry/src/pingpy/debug/statusStream.py
+++ raspberry/src/pingpy/debug/statusStream.py
@@ -11,29 +11,22 @@ class StatusStreamer:
         self.lastTime = 0
     
     def lookForSshIp(self):
-        print("INFO"+ __name__+"lookForSshIp"+ "Looking for SSH IP...")
         result = os.popen("who").read()  # Exécute la commande 'who'
-        print("INFO"+ __name__+"lookForSshIp"+ f"Result of 'who': {result}")
         for line in result.splitlines():
             parts = line.split()
             if len(parts) > 4 and parts[4].startswith("("):
-                print("INFO"+ __name__+"lookForSshIp"+ f"SSH IP found: {parts[4][1:-1]}")
                 return parts[4][1:-1]  # Enlève les parenthèses autour de l'IP
-        print("INFO"+ __name__+"lookForSshIp"+ "SSH IP not found")
         return None
     
     def sendStatus(self, input, t =  time.time()):
-        print("INFO"+ __name__+"sendStatus"+ "sendStatus at time"+ f"t: {t}")
         if t - self.lastTime < 0.5:
             return
         self.lastTime = t
-        print("INFO"+ __name__+"sendStatus to IP"+ f"sendToIP: {self.sendToIP}")
         
         if self.sendToIP is None:
             return
         
         playerPosition = [input.player[i].linearActuator.currentPose for i in range(4)]
-        print("INFO"+ __name__+"sendStatus"+ "send new player position")
 
         # replace None by 0
         playerPosition = [0 if p is None else p for p in playerPosition]
