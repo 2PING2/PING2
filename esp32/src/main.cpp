f3547b9e88a6d47f7fb8bd1c1a7281ebb1738825
robinAZERTY <robinlecouffe@gmail.com>
2025-02-07 17:30:17 +0100

    refactor: migrate from AccelStepper to FastAccelStepper in LinearActuator

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,19 +1,40 @@
+// #define EVERYTHING_PUBLIC
+// #include <Arduino.h>
+// #include "PING.hpp"
+
+// Player *players[4] = {&PING::player1, &PING::player2, &PING::player3, &PING::player4};
+
+// void setup()
+// {
+//     Serial.begin(115200);
+//     PING::setup();
+//     // Serial.println("hello from PING");
+// }
+
+// void loop()
+// {
+//     for (LinearActuator *la : LinearActuator::all)
+//         la->run();
+// }
+
 #define EVERYTHING_PUBLIC
 #include <Arduino.h>
-#include "PING.hpp"
+#include "LinearActuator.hpp"
 
-Player *players[4] = {&PING::player1, &PING::player2, &PING::player3, &PING::player4};
+LinearActuator la1(P1_STEP_PIN, P1_DIR_PIN, TMC1_ADDRESS, P1_INVERT_DIR);
 
 void setup()
 {
     Serial.begin(115200);
-    PING::setup();
-    // Serial.println("hello from PING");
+    LinearActuator::setup_all();
+    la1.setup();
+    Serial.println("hello from LinearActuator");
+    la1.move_to(100);
+    Serial.println("moved to 100");
+    la1.move_to(0);
+    Serial.println("moved to 0");
 }
 
 void loop()
 {
-    for (LinearActuator *la : LinearActuator::all)
-        la->run();
 }
-
