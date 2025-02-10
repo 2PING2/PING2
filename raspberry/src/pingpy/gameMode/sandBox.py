256e781fa2acc3d1fc33090e433bc84028aa9a40
robinAZERTY <robinlecouffe@gmail.com>
2025-02-10 17:36:33 +0100

    refactor: update maxSpeed and maxAcceleration parameters in config.h and SandBox class

--- raspberry/src/pingpy/gameMode/sandBox.py
+++ raspberry/src/pingpy/gameMode/sandBox.py
@@ -21,10 +21,10 @@ class SandBox(GameMode):
         # parameters
         self.maxPowerBumper = 1.0
         self.minPowerBumper = 0.2
-        self.maxSpeed = 700.0
+        self.maxSpeed = 1500.0
         self.minSpeed = 100.0
         self.minAcceleration = 600.0
-        self.maxAcceleration = 3500.0
+        self.maxAcceleration = 600.0
                 
     def setup(self, Input, Output):
         """
