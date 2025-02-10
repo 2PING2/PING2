b388d1e242667eedf6800a5f4979f84d9c73869b
robinAZERTY <robinlecouffe@gmail.com>
2025-02-10 17:50:09 +0100

    refactor: remove commented-out code and add debug print statement in Ping and SerialCom classes

--- raspberry/src/pingpy/ping.py
+++ raspberry/src/pingpy/ping.py
@@ -61,7 +61,8 @@ class Ping:
             self.input.player[i].linearActuator.computeInterpolation(timeStep, i)
             
         statusStreamer.sendStatus(self.input, t)
-            
+        print(self.playerController[3].connectedFlag)
+
         self.runGameMode()
         self.refresh_output()
 
